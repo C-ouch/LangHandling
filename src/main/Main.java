d944f776fce99c0f6207fe5773c35fbccaee0ee3
C-ouch <couchmec@clarkson.edu>
2022-05-05 20:34:17 -0400

    Start file

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+import javax.swing.text.AbstractDocument;
+import javax.swing.JTextArea;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
